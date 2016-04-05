af2c1b66a013be6ffbda1d27acb0aa0fe8cb1d77
Paul Brooker <pdbrooker@hotmail.com>
2016-04-05 08:56:02 -0300

    added id links to report found pet

--- app/src/main/java/com/codeitonce/petshout/ReportFoundPetFragment.java
+++ app/src/main/java/com/codeitonce/petshout/ReportFoundPetFragment.java
@@ -6,12 +6,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ReportFoundPetFragment extends Fragment {
+
+    private EditText mEmail;
+    private EditText mPhoneNumber;
+    private EditText mBreed;
+    private EditText mLocation;
+    private Spinner mSpecies;
+    private RadioGroup mGender;
+    private Button mAddImage;
 
 
     public ReportFoundPetFragment() {
@@ -23,7 +35,17 @@ public class ReportFoundPetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_found_pet, container, false);
+        View view = inflater.inflate(R.layout.fragment_report_found_pet, container, false);
+
+        mEmail = (EditText) view.findViewById(R.id.emailAddressInput);
+        mPhoneNumber = (EditText) view.findViewById(R.id.phoneNumberInput);
+        mBreed = (EditText) view.findViewById(R.id.breedInput);
+        mLocation = (EditText) view.findViewById(R.id.locationInput);
+        mSpecies = (Spinner) view.findViewById(R.id.speciesSpinner);
+        mGender = (RadioGroup) view.findViewById(R.id.gender_group);
+        mAddImage = (Button) view.findViewById(R.id.imageButton);
+
+        return  view;
     }
 
 }
