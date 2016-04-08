5d70978e9cccbd4a542cff594ad493c1abc26b94
Paul Brooker <pdbrooker@hotmail.com>
2016-04-08 16:34:46 -0300

    minor changes working towards building browse pets page

--- app/src/main/java/com/codeitonce/petshout/BrowsePetsFragment.java
+++ app/src/main/java/com/codeitonce/petshout/BrowsePetsFragment.java
@@ -7,11 +7,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BrowsePetsFragment extends Fragment {
+public class BrowsePetsFragment extends Fragment
+{
+
+    private ArrayList<Post> mPostArrayList;
 
 
     public BrowsePetsFragment() {
@@ -23,7 +28,14 @@ public class BrowsePetsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_browse_pets, container, false);
+        View view = inflater.inflate(R.layout.fragment_browse_pets, container, false);
+
+
+
+
+        return view;
     }
+
+
 
 }
