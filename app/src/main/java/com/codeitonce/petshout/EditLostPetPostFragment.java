0f98632b5bb5f34f62196d9209c4a663a6c21f07
Paul Brooker <pdbrooker@hotmail.com>
2016-04-09 14:34:22 -0300

    added spinner data for species to createPetProfile and editlostpetprofile

--- app/src/main/java/com/codeitonce/petshout/EditLostPetPostFragment.java
+++ app/src/main/java/com/codeitonce/petshout/EditLostPetPostFragment.java
@@ -6,12 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EditLostPetPostFragment extends Fragment {
+public class EditLostPetPostFragment extends Fragment
+{
+    private Spinner mSpecies;
+    private String species;
 
 
     public EditLostPetPostFragment() {
@@ -23,7 +28,67 @@ public class EditLostPetPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_lost_pet_post, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_lost_pet_post, container, false);
+
+        mSpecies = (Spinner) view.findViewById(R.id.species_spinner);
+
+
+        mSpecies.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                int num = position;
+                species = "";
+
+                switch (num)
+                {
+                    case 0:
+                        species = getString(R.string.dog);
+                        break;
+                    case 1:
+                        species = getString(R.string.cat);
+                        break;
+                    case 2:
+                        species = getString(R.string.bird);
+                        break;
+                    case 3:
+                        species = getString(R.string.rat);
+                        break;
+                    case 4:
+                        species = getString(R.string.hedgehog);
+                        break;
+                    case 5:
+                        species = getString(R.string.pig);
+                        break;
+                    case 6:
+                        species = getString(R.string.horse);
+                        break;
+                    case 7:
+                        species = getString(R.string.reptile);
+                        break;
+                    case 8:
+                        species = getString(R.string.guinea_pig);
+                        break;
+                    case 9:
+                        species = getString(R.string.rabbit);
+                        break;
+                    case 10:
+                        species = getString(R.string.other);
+                        break;
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
+        });
+
+        return view;
     }
 
 }
